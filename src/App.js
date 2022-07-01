6816d41762a2fa59e06c3d16e64e6f60c7f5e6a5
jonatas vaz <santosjonaras@gmail.com>
2022-07-01 16:02:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css"
+import { useState } from "react";
+import { HiSearch } from "react-icons/hi";
 
 function App() {
+ const [ city,setCity] =useState("brasilia")
+ const [weatherForecast, setWeatherForecast] = useState(null);
+
+const  handleChange = (e)=>{
+setCity(e.target.value)
+}
+const hadleSearch= ()=>{
+  fetch(`http://api.weatherapi.com/v1/current.json?key=b99cfb7550f74145923235454223006&q=${city}&lang=pt`)
+  .then((response )=>{
+  
+     if(response.status===200){
+  
+       return response.json()
+     }
+  })
+  .then((data)=>{
+    console.log(data)
+    setWeatherForecast(data);
+    
+  })
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+       <div className="container">
+           <nav className="nav">
+             <button className="btn-search" onClick={hadleSearch}><HiSearch/></button>  
+             <input type="text" onChange={handleChange} value={city}/> 
+            </nav>
+            
+            
+            <main>
+       
+            {weatherForecast ? (
+            <>
+              <div className="geral">
+                 <div>
+                   <h1 className="name-city">
+                   {weatherForecast.location.region}
+                 </h1>
+                 </div>
+                 
+                <div className="img">
+                  <img
+                    src={`${weatherForecast.current.condition.icon}`}
+                    alt="Weather Icon"
+                  />
+                </div>
+                <div>
+                <h3 className="clima">
+                     {weatherForecast.current.condition.text}
+                  </h3>
+                  <p className="lead">
+                    Temperatura: {weatherForecast.current.temp_c}&#8451;
+                  </p>
+                  <h3 className="humidity">
+                  humidade: {weatherForecast.current.humidity}
+                  </h3>
+                  <h3 className="pressao"> pressão: {weatherForecast.current.pressure_mb}mbar</h3>
+                </div>
+              </div>
+            </>
+          ) : null}
+   
+            
+            </main>
+       </div>
+
+            
+
+     </div>
   );
 }
 
